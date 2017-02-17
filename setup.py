fc344253464b4dcd4ded9f010ede65ecf7afbcca
cfrioux <clemence.frioux@irisa.fr>
2017-02-17 20:04:26 +0100

    fixed typo

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 #
 # This file is part of MeneTools.
 #
-# meneco is free software: you can redistribute it and/or modify
+# MeneTools is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# meneco is distributed in the hope that it will be useful,
+# MeneTools is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
@@ -25,7 +25,7 @@ setup(
   description      = 'Metabolic Network Topology Tools. Analyze the topology \
                       of metabolic networks. Explore producibility, production \
                       paths and needed initiation sources',
-  long_description = open('README').read(),
+  long_description = open('README.md').read(),
   author           = 'Clemence Frioux',
   author_email     = 'clemence.frioux@inria.fr',
   packages         = ['menetools'],
