73b607de511b86cc22ec2a75087b8aed8a4dda9e
cfrioux <clemence.frioux@irisa.fr>
2017-08-23 16:48:56 +0200

    fixed in-dev tools mentions in setup.py

--- setup.py
+++ setup.py
@@ -19,20 +19,20 @@ from setuptools import setup
 
 setup(
     name             = 'MeneTools',
-    version          = '1.0.3',
+    version          = '1.0.4',
     url              = 'https://github.com/cfrioux/MeneTools',
     download_url     = 'https://github.com/cfrioux/MeneTools/tarball/1.0.3',
     license          = 'GPLv3+',
     description      = 'Metabolic Network Topology Analysis Tools',
     long_description = 'Python 3 Metabolic Network Topology Tools. Analyze the \
 topology of metabolic networks. Explore producibility, production paths and \
-needed initiation sources\
+needed initiation sources. \
 More information on usage and troubleshooting on Github: https://github.com/cfrioux/MeneTools',
     author           = 'Clemence Frioux',
     author_email     = 'clemence.frioux@gmail.com',
     packages         = ['menetools'],
     package_dir      = {'menetools' : 'src'},
     package_data     = {'menetools' : ['encodings/*.lp']},
-    scripts          = ['menecof.py','menescope.py','menepath.py','menecheck.py', 'meneseed.py', 'mene2asp.py'],
+    scripts          = ['menecof.py','menescope.py','menepath.py','menecheck.py'],
     install_requires = ['pyasp == 1.4.3']
 )
