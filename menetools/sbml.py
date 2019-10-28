991caf019d945d3b5036c2dad2905b8d78d7d9e8
Arnaud Belcour <arnaud.belcour@irisa.fr>
2019-10-28 17:42:36 +0100

    Fix issue with targets SBML without species.

--- menetools/sbml.py
+++ menetools/sbml.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import re
+import sys
 from  clyngor import as_pyasp
 from  clyngor.as_pyasp import TermSet, Atom
 import xml.etree.ElementTree as etree
@@ -225,13 +226,16 @@ def readSBMLspecies_clyngor(filename, speciestype) :
     model = get_model(sbml)
 
     listOfSpecies = get_listOfSpecies(model)
-    for e in listOfSpecies:
-        if e.tag[0] == "{":
-            uri, tag = e.tag[1:].split("}")
-        else:
-            tag = e.tag
-        if tag == "species":
-            all_atoms.add(Atom(speciestype, ["\""+e.attrib.get("id")+"\""]))
+    if listOfSpecies:
+        for e in listOfSpecies:
+            if e.tag[0] == "{":
+                uri, tag = e.tag[1:].split("}")
+            else:
+                tag = e.tag
+            if tag == "species":
+                all_atoms.add(Atom(speciestype, ["\""+e.attrib.get("id")+"\""]))
+    else:
+        sys.exit("Invalid SBML (missing species or listOfSpecies) " + filename)
 
     lpfacts = TermSet(all_atoms)
 
