7155e9121477913e15980ae94651f9b58ea74893
Clémence Frioux <cfrioux@users.noreply.github.com>
2020-03-26 17:46:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     name             = 'MeneTools',
     version          = '2.0.4',
     url              = 'https://github.com/cfrioux/MeneTools',
-    download_url     = 'https://github.com/cfrioux/MeneTools/tarball/1.0.4',
+    download_url     = 'https://github.com/cfrioux/MeneTools/tarball/2.0.4',
     license          = 'GPLv3+',
     description      = 'Metabolic Network Topology Analysis Tools',
     long_description = 'Python 3.6 Metabolic Network Topology Tools. Analyze the \
