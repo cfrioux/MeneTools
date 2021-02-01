fddbb3177f6e311c7d4db5f69b99071d408854b5
cfrioux <clemence.frioux@gmail.com>
2021-02-01 15:15:04 +0100

    update license headers in all files

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
-# Copyright (c) 2017, Clemence Frioux <clemence.frioux@inria.fr>
-#
-# This file is part of MeneTools.
-#
-# MeneTools is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2017-2021 Clémence Frioux & Arnaud Belcour - Inria Dyliss - Pleiade
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-#
-# MeneTools is distributed in the hope that it will be useful,
+
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with menetools.  If not, see <http://www.gnu.org/licenses/>.
-# -*- coding: utf-8 -*-
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
 from setuptools import setup
 import menetools
 
