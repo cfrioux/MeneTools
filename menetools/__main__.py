1660a59e04ed3eae550e971fb12fa5b2b6ffc10c
Clémence Frioux <cfrioux@users.noreply.github.com>
2021-02-01 15:03:22 +0100

    update licence in main

--- menetools/__main__.py
+++ menetools/__main__.py
@@ -21,10 +21,19 @@ from menetools.menedead import run_menedead
 from shutil import which
 
 VERSION = pkg_resources.get_distribution("menetools").version
-LICENSE = """Copyright (C) Dyliss - Pleiade
-License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
-MeneTools is free software: you are free to change and redistribute it.
-There is NO WARRANTY, to the extent permitted by law.\n
+LICENSE = """Copyright (C) Clémence Frioux & Arnaud Belcour - Inria Dyliss - Pleiade
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>\n.
 """
 MESSAGE = """
 Explore the producibility potential in a metabolic network using the network expansion algorithm.
